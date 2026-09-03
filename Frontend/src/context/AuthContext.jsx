7caa745f1600a50c752f52db3ec22bc40af0c0dc
sauravsultane <sultanesaurav1.com>
2026-09-03 13:46:48 +0530

    bug fix in context

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useEffect, useContext } from 'react';
-import api from '../services/api';
+import api from '../services/api.js';
 
 export const AuthContext = createContext();
 
